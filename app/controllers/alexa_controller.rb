65dcd07bc685e0e953b00d5b276d21cb430cfef9
Nathan Brenner <nathan.j.brenner@gmail.com>
2021-07-09 15:16:06 -0700

    initialized route

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -1,2 +1,10 @@
 class AlexaController < ApplicationController
+	skip_before_action :verify_authenticity_token
+	def create
+		render json: {"data": "hello world"}
+	end
+
+	def index
+		render json: { "data": "todo: documentation on how to use the skill"}
+	end
 end
