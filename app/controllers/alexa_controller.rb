4a7420d3a2862cefedfd7965aa71e775a033f6dd
Nathan Brenner <nathan.j.brenner@gmail.com>
2021-07-09 16:06:46 -0700

    session start

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -1,7 +1,24 @@
 class AlexaController < ApplicationController
 	skip_before_action :verify_authenticity_token
 	def create
-		render json: {"data": "hello world"}
+    message = "hello world"
+    session_attributes = {"previous_session": "something"}
+    session_end = true
+
+		case params[:request][:type]
+		when 'LaunchRequest'
+			message = 'Hello! Welcome to Cake time. What is your birthday?'
+		end
+    render json: {
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": message,
+        },
+        "shouldEndSession": session_end
+      },
+      "sessionAttributes": session_attributes
+    }
 	end
 
 	def index
